7f7e21f5a0fa552f3077156df9d450d881b0a60a
Alexey Tarasov <alexey.tarasov@qoden.net>
2023-07-12 14:15:57 +0200

    WIP

--- 11.irregular_verbs/index.sh
+++ 11.irregular_verbs/index.sh
@@ -11,8 +11,13 @@ while true; do
     # bring, brought, brought — приносить
     line=$(cat verb.txt | shuf | head -n 1)
 
-    line1=$(echo "$line" | awk '{print $1}')
-    line2=$(echo "$line" | awk '{print $3}')
+    row1=$(echo "$line" | awk '{print $1}')
+    row2=$(echo "$line" | awk '{print $2}')
+    row3=$(echo "$line" | awk '{print $3}')
+    row4=$(echo "$line" | awk '{print $4}')
+    row5=$(echo "$line" | awk '{print $5}')
+    row6=$(echo "$line" | awk '{print $6}')
+    row7=$(echo "$line" | awk '{print $7}')
     # line3=$(echo $line | cut -f 3 -d "," | cut -f 1 -d "—")
     # line4=$(echo $line | cut -f 3 -d "," | cut -f 2 -d "—")
     # Запрос ввода от пользователя
@@ -27,10 +32,11 @@ while true; do
     #     echo "Введенное значение не совпадает с ожидаемым."
     # fi
 
-    while [ "$userInput" != "$line2" ]; do
-        echo "Введите неправильную вторую форму глагола $line1"
+    while [ "$userInput" != "$row3" ]; do
+        echo "Введите прошлое время (вторую форму) глагола $row1"
         # echo "ожидаю $line2"
         read userInput
     done
 
+    echo "Правильно, $row3 $row4 сам глагол $row7"
 done
