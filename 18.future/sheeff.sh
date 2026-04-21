5c012883a11ff80d0ade20739f4a5672fd08ee57
Alexey Tarasov <alexey.tarasov@perfectart.com>
2026-04-21 21:34:12 +0000

    feat: add future

---
+++ 18.future/sheeff.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# https://www.learnenglishbest.com/tobe/
+
+# set -ex
+
+# выставляем цифру сколько файлов делать
+COUNT_OF_FILES=1
+
+
+# Название для итогового файла
+r='result.txt'
+
+# Переменную, откуда будем время брать
+# предположения - по моему мнению - p
+# предположения - текущая ситуация движется к тому, что это случится - P
+# намерения - только что пришла в голову идея и ее хочешь сделать - H
+# намерения - запланировал что-то всерьез и надолго - h
+case='h H P p'
+#time='= = ='
+
+# Переменную, откуда будем знак брать
+# sign='+ - ?'
+
+fverb=''
+# Пофайлово делаем:
+for ((k = 0; k < ${COUNT_OF_FILES}; k++)); do
+
+    # Удалем из названия файла цифру его номера предыдущую
+    r=$(echo ${r} | sed 's/[0-9]*//g')
+
+    # Ставим цифру текущую
+    r=${k}${r}
+
+    # Чистим файл на всякий случай, если он вдруг был
+    echo -n "" >$r
+
+    for ((i = 0; i < 100; i++)); do
+
+        echo "Do ${i} item of ${k} file"
+
+        # # Пишем в файл при этом выбираем случайное слово и без переноса строк всё
+        # echo $sign | cut -f $(echo $(($RANDOM % 3 + 1))) -d " " | tr -d '\r\n' >>$r
+        # echo -n "     " >>$r
+
+
+
+        # echo $fverb | cut -f $(echo $(($RANDOM % 6 + 1))) -d " " | tr -d '\r\n' >>$r
+        # echo -n "     " | tr -d '\r\n' >>$r
+
+        # echo -n '_|_' >>$r
+        # echo -n "     " | tr -d '\r\n' >>$r
+
+        echo -n $(cat noun.txt | shuf | head -n 1) | tr -d '\r\n' >>$r
+        echo -n "     " | tr -d '\r\n' >>$r
+
+        echo $case | cut -f $(echo $(($RANDOM % 4 + 1))) -d " " | tr -d '\r\n' >>$r
+        echo -n "     " >>$r
+
+        echo -n $(cat verb.txt | shuf | head -n 1) | tr -d '\r\n' >>$r
+        # Добавляем перенос строк, чтобы завершить строку
+        echo -n "     " | tr -d '\r\n' >>$r
+
+
+        echo -n $(cat noun.txt | shuf | head -n 1) | tr -d '\r\n' >>$r
+        echo "     " >>$r
+
+        echo "" >>$r
+        echo "" >>$r
+        echo "" >>$r
+
+    done
+
+done
